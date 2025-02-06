18df425f9212edeb9a30250824a9b39eb2de357c
ZulmaNataliaTabare <sulmatabare@gmail.com>
2025-02-06 19:03:59 -0300

    Acomodando los estilos

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -10,19 +10,8 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Servir el servidor en el puerto 3001
-app.listen(3001, () => {
-    console.log('Servidor corriendo en http://localhost:3001');
-});
 
 
-// controla el menú hamburguesa
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navMenu = document.querySelector('.main-nav');
-
-hamburgerMenu.addEventListener('click', () => {
-    navMenu.classList.toggle('open');
-});
 
 
 
